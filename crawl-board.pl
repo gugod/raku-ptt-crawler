e6c41fc59a883396725101c8568765adc57164bd
Kang-min Liu <gugod@gugod.org>
2015-09-24 09:08:29 +0200

    implement harvest_articles

--- crawl-board.pl
+++ crawl-board.pl
@@ -17,6 +17,26 @@ sub ptt_get($url) {
     }
 }
 
+sub download_articles(@articles, $output_dir) {
+    for @articles -> $a {
+        say $a;
+    }
+}
+
+sub harvest_articles(Str $url, Str $board_name) {
+    say "$url == $board_name";
+    my $res = ptt_get($url);
+    my $html = $res.content;
+    my $dom = HTML::Parser::XML.new.parse($html);
+    my @articles;
+    for $dom.elements(:TAG<a>, :RECURSE<99>, :href) -> $el {
+        if (my $href = $el.attribs<href>) ~~ rx/ M \. <[0..9]>+ \. A \. <[A..Z0..9]>**3 \.html $/ {
+            @articles.push: ${ :subject($el.contents.join), :url(PTT_URL ~ $href) };
+        }
+    }
+    return @articles;
+}
+
 sub harvest_board_indices(Str $board_url, Str $board_name) {
     my $res = ptt_get($board_url);
     my $html = $res.content;
@@ -25,12 +45,12 @@ sub harvest_board_indices(Str $board_url, Str $board_name) {
     my @boards;
     for $dom.elements(:TAG<a>, :RECURSE<99>, :href) -> $el {
         if (my $href = $el.attribs<href>) ~~ rx/index (<[0..9]>+) \.html $/ {
-            @boards.push: ${ :page_number($0), :url($href) };
+            @boards.push: ${ :page_number($0), :url(PTT_URL ~ $href) };
         }
     };
     @boards = @boards[1,0] if @boards[0]<page_number> > @boards[1]<page_number>;
     for @boards[0]<page_number> .. @boards[1]<page_number> -> $n {
-        @boards.push: ${ :page_number($n), :url("/bbs/$board_name/index$n.html" ) }
+        @boards.push: ${ :page_number($n), :url( PTT_URL ~ "/bbs/$board_name/index$n.html" ) }
     }
     return @boards;
 }
@@ -42,8 +62,10 @@ sub main(Str $board_name, Str $output_dir) {
     mktree( $output_board_dir );
 
     my @board_indices = harvest_board_indices($board_url, $board_name);
-
-    .say for @board_indices;
+    for @board_indices.sort({ $^b.<page_number> <=> $^a.<page_number> }) -> $board {
+        my @articles = harvest_articles($board.<url>, $board_name);
+        download_articles(@articles, $output_board_dir);
+    }
 }
 
 main(@*ARGS[0], @*ARGS[1]);
